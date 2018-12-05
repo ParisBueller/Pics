166d1dbb8d28d8ced8ffd6fc0311ed911e2f9f4d
Paris Ward <wardparis.pw@gmail.com>
2018-12-05 09:51:41 -0800

    rendered list of results from search, added refs to grid for proper styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,8 +3,12 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = { term: ''};
 
+    //in general everytime you have a callback function, use an arrow function
+    //will automaticall bind the function to have the appropriate value of 'this'
     onFormSubmit = e => {
         e.preventDefault();
+        //in order to communicate from child to parent, we pass a callback from
+        //parent to child , and the child will call that callback
         this.props.onSubmit(this.state.term);
     };
 
